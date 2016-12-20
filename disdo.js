7490f8f3af578f1c2efcf60bc81eda11547cf97e
johnlenon <johnlenonmaghanoy@gmail.com>
2016-12-20 13:18:23 +0800

    Improve build procedure.

--- disdo.js
+++ disdo.js
@@ -34,6 +34,9 @@
 			"file": "disdo.js",
 			"module": "disdo",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/disdo.git",
 			"test": "disdo-test.js",
