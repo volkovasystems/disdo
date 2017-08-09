fe9f107d8e13ab1daa2514dbc8cf1aee0531dd8b
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-09 12:31:21 +0800

    Updated test.

--- disdo-test.js
+++ disdo-test.js
@@ -2,14 +2,14 @@
 const assert = require( "assert" );
 const disdo = require( "./disdo.js" );
 
-assert.equal( disdo( "hello-world" ), "hello world", "should have value 'hello world'" );
+assert.equal( disdo( "hello-world" ), "hello world", "should be equal to 'hello world'" );
 
-assert.equal( disdo( "hello_world" ), "hello world", "should have value 'hello world'" );
+assert.equal( disdo( "hello_world" ), "hello world", "should be equal to 'hello world'" );
 
-assert.equal( disdo( "helloWorld" ), "hello World", "should have value 'hello world'" );
+assert.equal( disdo( "helloWorld" ), "hello World", "should be equal to 'hello World'" );
 
-assert.equal( disdo( "hello world" ), "hello world", "should have value 'hello world'" );
+assert.equal( disdo( "hello world" ), "hello world", "should be equal to 'hello world'" );
 
-assert.equal( disdo( "hello World" ), "hello World", "should have value 'hello World'" );
+assert.equal( disdo( "hello World" ), "hello World", "should be equal to 'hello World'" );
 
 console.log( "ok" );
